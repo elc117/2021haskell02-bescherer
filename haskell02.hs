bc331c360275a1cc6ab576fd88384576e3b623f7
bescherer <>
2021-05-26 21:34:35 +0000

    Tarefas 02 de Haskell

--- haskell02.hs
+++ haskell02.hs
@@ -28,11 +28,15 @@ quarentena :: [(String, Float)] -> [(String, Float)]
 quarentena x = filter (\(_,x) -> hasFebre x) x
 
 --6
---idadesEm :: [Int] -> Int -> [Int]
+idadesEm :: [Int] -> Int -> [Int]
+idadesEm x y = map (\c -> y - c) x
 
 --7
---changeNames :: [String] -> [String]
+headisA :: String -> String
+headisA x = if head x == 'A' then "Super" ++ x else x
 
+changeNames :: [String] -> [String]
+changeNames x = map headisA x
 --8
 onlyShorts :: [String] -> [String]
 onlyShorts x = filter (\x -> length x < 5) x
